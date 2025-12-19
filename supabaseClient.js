b4c9b7f9db80e56b2bcef86423585f65019cadbe
KatherineDelaney <157722526+KatherineDelaney@users.noreply.github.com>
2025-12-19 14:10:19 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,18 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
-// import dotenv from "dotenv";
-
-// Ensure dotenv is configured here as well to be safe
-// dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-// Log to help you troubleshoot (you can remove this later)
+
 if (!supabaseUrl || !supabaseKey) {
   console.error("❌ ERROR: Supabase environment variables are missing!");
-  console.log("Current Directory:", process.cwd());
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabase = createClient(supabaseUrl || '', supabaseKey || '');
 
 export default supabase;
