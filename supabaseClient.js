298f0289973e143cfd43e588fe0c0beb6874b184
KatherineDelaney <157722526+KatherineDelaney@users.noreply.github.com>
2025-12-19 20:12:06 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,12 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-
 if (!supabaseUrl || !supabaseKey) {
   console.error("❌ ERROR: Supabase environment variables are missing!");
 }
 
-
-const supabase = createClient(supabaseUrl || '', supabaseKey || '');
+const supabase = createClient(supabaseUrl || "", supabaseKey || "");
 
 export default supabase;
